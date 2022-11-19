02159409de7f706df8f0c619a71a347ed445920b
Kiran Rai <travestroy007@gmail.com>
2022-11-19 11:38:26 +0545

    adding cors and cors options

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,7 +1,7 @@
 const { format } = require("date-fns");
 const { v4: uuid } = require("uuid");
 const fs = require("fs");
-const fsPromises = required("fs").promises;
+const fsPromises = require("fs").promises;
 const path = require("path");
 
 const logEvents = async (message, logFileName) => {
